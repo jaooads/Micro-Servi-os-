f73f51b16503e84d18bba12606e004b1fbe0d399
jaooads <136362624+jaooads@users.noreply.github.com>
2024-02-13 17:46:32 -0300

    Sessão 2 - Aula 10 até aula 23

---
+++ src/PostCreate.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default () => {
+    return <div>
+        <form>
+            <div className="form-group">
+                <label>Tittle</label>
+                <input className="form-control" />
+            </div>
+            <button className="btn btn-primary">Enviar</button>
+        </form>
+    </div>
+}

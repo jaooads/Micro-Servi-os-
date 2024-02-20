f0c9a5ca0c4828e025b8903a83c1422287b36e88
jaooads <136362624+jaooads@users.noreply.github.com>
2024-02-20 07:53:07 -0300

    Add Comment Moderation

--- src/CommentList.js
+++ src/CommentList.js
@@ -1,24 +1,12 @@
-import React, { useState, useEffect } from "react";
-import axios from 'axios';
+import React from "react";
 
-const CommentList = ({ postId }) => {
-    const [comments, setComments] = useState([]);
 
-    const fetchData = async () => {
-        const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`);
-
-        setComments(res.data);
-    };
-
-    useEffect(() => {
-        fetchData();
-    }, []); // [] Serve para chamar o evento apenas uma unica vez
+export default ({ comments }) => {
 
     const renderedComments = comments.map((comment) => {
+
         return <li key={comment.id}>{comment.content}</li>; //Retorna o conteudo de um comentario
     });
 
     return <ul>{renderedComments}</ul>;
-};
-
-export default CommentList;  
+}
--- src/PostList.js
+++ src/PostList.js
@@ -7,9 +7,10 @@ const PostList = () => {
     const [posts, setPosts] = useState({}); //Declarando  //useState() Define o valor inicial em 0, pois estamos
     //o valor de um post
     const fetchPosts = async () => {
-        const res = await axios.get('http://localhost:4000/posts');   //Localização para o localhost sobre os posts
-
-        setPosts(res.data);
+        const res = await axios.get('http://localhost:4002/posts');   //Localização para o localhost sobre os posts
+        console.log(res.data);     
+        setPosts(res.data); //Ela está fazendo toda hora solicitações para obter toda a lista de comentários
+                            // que não são mais necessários 
 
         //Definir quando iremos chamar a função
         //Define a função apenas quando a solicitação do Fetch retornar algo;
@@ -19,7 +20,7 @@ const PostList = () => {
         fetchPosts();
     }, []);
     console.log(posts);
-         // Array vazio serve para dizer que está função será executada apenas uma vez
+    // Array vazio serve para dizer que está função será executada apenas uma vez
     const renderPosts = Object.values(posts).map(post => {
         return (
             <div
@@ -29,7 +30,7 @@ const PostList = () => {
             >
                 <div className="card-body">
                     <h3>{post.title}</h3>
-                    <CommentList postId={post.id} />
+                    <CommentList comments={post.comments} />
                     <CommentCreate postId={post.id} />
 
                 </div>
